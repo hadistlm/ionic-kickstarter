a5816027381ff3d8f846cee5251136e7ed51d363
Islaa <hadist.lm@gmail.com>
2021-03-04 15:53:15 +0700

    Adding : base functio to connection service & helper

--- src/app/service/connection.service.ts
+++ src/app/service/connection.service.ts
@@ -1,9 +1,228 @@
 import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { HTTP } from '@ionic-native/http/ngx';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { File } from '@ionic-native/file/ngx';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+
+import { environment } from 'src/environments/environment';
+import { HelperService } from 'src/app/service/helper.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectionService {
+	private url: string = environment.baseURL;
+	private headers={
+       'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+      ,'Accept':'application/json, text/plain'
+      ,'Access-Control-Allow-Methods':'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+      ,'Access-Control-Allow-Origin':'*'
+      ,'Access-Control-Allow-Credentials':'true'
+      ,'Authorization':((localStorage.getItem('authToken')) ? localStorage.getItem('authToken') : '')
+      ,'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, "Authorization"'
+  };
 
-  constructor() { }
+  constructor(
+  	private baseHTTP: HttpClient,
+  	private nativeHTTP: HTTP,
+  	private transfer: FileTransfer,
+  	public file:File,
+  	public helper: HelperService,
+    public platform: Platform
+  ) { }
+
+  get(endpoint: string, data?: any) {
+  	if (localStorage.getItem('authToken')) {
+      this.headers.Authorization=localStorage.getItem('authToken');
+    };
+
+  	return new Promise((resolve,reject)=>{
+  		// Select device activated
+      this.platform.ready().then(() => {
+        // device is mobile
+        if (this.platform.is('ios') || this.platform.is('android')) {
+          // init file transfer
+          this.nativeHTTP.get(this.url + '/' + endpoint, data, this.headers).then(success=>{
+		        resolve(success);
+		      },err=>{
+		        this.setTokenExpired(err);	
+		        reject(err);
+		      });
+
+        // device is webapp
+        }else{
+        	this.baseHTTP.get(this.url + '/' + endpoint, {params: data, headers: this.headers}).subscribe((response) => {
+	          resolve(response);
+	        }, (error) => {
+	          this.setTokenExpired(error);	
+		        reject(error);
+	        });
+        };
+      });
+    });
+  }
+
+  post(endpoint: string, body?: any) {
+  	if (localStorage.getItem('authToken')) {
+      this.headers.Authorization=localStorage.getItem('authToken');
+    };
+
+  	return new Promise((resolve,reject)=>{
+  		// Select device activated
+      this.platform.ready().then(() => {
+        // device is mobile
+        if (this.platform.is('ios') || this.platform.is('android')) {
+          // init file transfer
+          this.nativeHTTP.post(this.url + '/' + endpoint, body, this.headers).then(success=>{
+		        resolve(success);
+		      },err=>{
+		        this.setTokenExpired(err);	
+		        reject(err);
+		      });
+
+        // device is webapp
+        }else{
+        	this.baseHTTP.post(this.url + '/' + endpoint, {params: body, headers: this.headers}).subscribe((response) => {
+	          resolve(response);
+	        }, (error) => {
+	          this.setTokenExpired(error);	
+		        reject(error);
+	        });
+        };
+      });
+    });
+  }
+
+  put(endpoint: string, body?: any) {
+  	if (localStorage.getItem('authToken')) {
+      this.headers.Authorization=localStorage.getItem('authToken');
+    };
+
+  	return new Promise((resolve,reject)=>{
+  		// Select device activated
+      this.platform.ready().then(() => {
+        // device is mobile
+        if (this.platform.is('ios') || this.platform.is('android')) {
+          // init file transfer
+          this.nativeHTTP.put(this.url + '/' + endpoint, body, this.headers).then(success=>{
+		        resolve(success);
+		      },err=>{
+		        this.setTokenExpired(err);	
+		        reject(err);
+		      });
+
+        // device is webapp
+        }else{
+        	this.baseHTTP.put(this.url + '/' + endpoint, {params: body, headers: this.headers}).subscribe((response) => {
+	          resolve(response);
+	        }, (error) => {
+	          this.setTokenExpired(error);	
+		        reject(error);
+	        });
+        };
+      });
+    });
+  }
+
+  delete(endpoint: string, body: any, reqOpts?: any) {
+  	if (localStorage.getItem('authToken')) {
+      this.headers.Authorization=localStorage.getItem('authToken');
+    };
+
+  	return new Promise((resolve,reject)=>{
+  		// Select device activated
+      this.platform.ready().then(() => {
+        // device is mobile
+        if (this.platform.is('ios') || this.platform.is('android')) {
+          // init file transfer
+          this.nativeHTTP.delete(this.url + '/' + endpoint, body, this.headers).then(success=>{
+		        resolve(success);
+		      },err=>{
+		        this.setTokenExpired(err);	
+		        reject(err);
+		      });
+
+        // device is webapp
+        }else{
+        	this.baseHTTP.delete(this.url + '/' + endpoint, {params: body, headers: this.headers}).subscribe((response) => {
+	          resolve(response);
+	        }, (error) => {
+	          this.setTokenExpired(error);	
+		        reject(error);
+	        });
+        };
+      });
+    });
+  }
+
+  patch(endpoint: string, body: any, reqOpts?: any) {
+  	if (localStorage.getItem('authToken')) {
+      this.headers.Authorization=localStorage.getItem('authToken');
+    };
+
+  	return new Promise((resolve,reject)=>{
+  		// Select device activated
+      this.platform.ready().then(() => {
+        // device is mobile
+        if (this.platform.is('ios') || this.platform.is('android')) {
+          // init file transfer
+          this.nativeHTTP.patch(this.url + '/' + endpoint, body, this.headers).then(success=>{
+		        resolve(success);
+		      },err=>{
+		        this.setTokenExpired(err);	
+		        reject(err);
+		      });
+
+        // device is webapp
+        }else{
+        	this.baseHTTP.patch(this.url + '/' + endpoint, {params: body, headers: this.headers}).subscribe((response) => {
+	          resolve(response);
+	        }, (error) => {
+	          this.setTokenExpired(error);	
+		        reject(error);
+	        });
+        };
+      });
+    });
+  }
+
+  download(endpoint,name) {
+    return new Promise((resolve,reject)=>{
+      this.platform.ready().then(() => {
+        // only process when device is mobile
+        if (this.platform.is('ios') || this.platform.is('android')) {
+          // init file transfer
+          const fileTransfer: FileTransferObject = this.transfer.create();
+          // set folder directory
+          const folder:string = this.file.externalRootDirectory + '/Download/';
+          // try to download
+          fileTransfer.download(this.url + '/' + endpoint, folder+""+name).then((entry) => {
+            console.log('mydownload complete: ',entry.toURL());
+            resolve(entry.toURL());    
+          }, (error) => {
+            console.log('mydownload error: ',error);
+            reject(error);
+          });
+
+        }else{
+          reject('false');
+        }
+      });
+    });
+  }
+
+  setTokenExpired(err){
+    let myTokn = localStorage.getItem('authToken');
+
+    if(myTokn!=undefined && myTokn!=""){
+        if(err.status==401){
+          // destroy existing data first
+          this.helper._destroySession().then(() => {
+            // redirect to login page
+            this.helper.openPage('login');
+          });
+        }
+    }
+  }	
 }
--- src/app/service/helper.service.ts
+++ src/app/service/helper.service.ts
@@ -1,9 +1,126 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
+	public version="V 0.0.1";
 
-  constructor() { }
+	public _user={
+    authToken:"",
+    id_users:"",
+    id_role:"",
+    id_vendor:"",
+    data:{},
+  };
+
+  constructor(
+  	public router:Router
+  ) { }
+
+  _generateID(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  _isJson(json_string){
+    json_string = typeof json_string !== "string"
+        ? JSON.stringify(json_string)
+        : json_string;
+
+    try {
+        json_string = JSON.parse(json_string);
+    } catch (e) {
+        return false;
+    }
+
+    if (typeof json_string === "object" && json_string !== null) {
+        return true;
+    }
+
+    return false;
+  }
+
+  _ucFirst(string) {
+    return (string) ? string[0].toUpperCase() + string.slice(1) : '';
+  }
+
+  openPage(location,param:any = {}){
+    let navigationExtras: NavigationExtras= {
+      queryParams: {
+        special: JSON.stringify(param)
+      }
+    };
+
+    this.router.navigate([location], navigationExtras);
+  }
+
+  countObject(data:object = {}){
+    return (data) ? Object.keys(data).length : 0;
+  }
+
+  sumArrayMulti(data:any){
+    const holder = Object.keys(data).map(function(key, index) {
+      return data.length
+    });
+
+    return holder.reduce(function (a, b) { return a + b; }, 0);
+  }
+
+  searchArray(key,nameKey, myArray){
+    for (let i=0; i < myArray.length; i++) {
+      if (myArray[i][key] === nameKey) {
+          return i;
+      }
+    }
+    return -1;
+  }
+
+  existArray(nameKey, myArray){
+    for (let i=0; i < myArray.length; i++) {
+      if (myArray[i] === nameKey) {
+          return true;
+      }
+    }
+    return false;
+  }
+
+  am_pm(date){
+    if(date.getHours()==0 && date.getMinutes()==0 && date.getSeconds()==0)
+      return ''; // No AM for MidNight
+    if(date.getHours()==12 && date.getMinutes()==0 && date.getSeconds()==0)
+      return ''; // No PM for Noon
+    if(date.getHours()<12)
+      return ' AM';
+    return ' PM';
+  }
+
+  dateConvert(date:string, format:string = 'YYYY-MM-DD HH-mm-ss'){
+    return moment(date).format(format);
+  }
+
+  dateIsEqual(date:string, type:any = 'day'){
+    return moment().isSame(date, type);
+  }
+
+  getToday(format:string = 'YYYY-MM-D'){
+    return moment().format(format);
+  }
+
+  getPastDay(numberOfDay:number = 1, format:string = 'YYYY-MM-D') {
+    return moment().subtract(numberOfDay, 'day').format(format);
+  }
+
+  date_format(date){
+    let result=this.setZero(date.getDate())+'/'+this.setZero(date.getMonth()+1)+'/'+(date.getFullYear()+' ').substring(2)+this.setZero(this.hours(date))+':'+this.setZero(date.getMinutes())+this.am_pm(date);
+    return result;
+  }
 }
